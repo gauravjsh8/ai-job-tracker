cdd16db4ff636d95417a2236def33259b0efa4ac
gauravjsh8 <gauravjsh8@gmail.com>
2026-04-30 16:01:52 +0300

    added logout functionality

--- backend/src/controllers/userController.ts
+++ backend/src/controllers/userController.ts
@@ -79,47 +79,79 @@ export const registerUser = async (
 };
 
 export const login = async (req: Request<{}, {}, LoginBody>, res: Response) => {
-  const { email, password } = req.body;
-  if (!email || !password) {
-    return res
-      .status(400)
-      .json({ success: false, message: "All fields are required" });
+  try {
+    const { email, password } = req.body;
+    if (!email || !password) {
+      return res
+        .status(400)
+        .json({ success: false, message: "All fields are required" });
+    }
+
+    const existingUser = await User.findOne({ email }).select("+password");
+    if (!existingUser) {
+      return res
+        .status(401)
+        .json({ success: false, message: "Invalid credentials" });
+    }
+
+    const isPasswordMatch = await bcrypt.compare(
+      password,
+      existingUser.password,
+    );
+    if (!isPasswordMatch) {
+      return res
+        .status(401)
+        .json({ success: false, message: "Invalid credentials" });
+    }
+
+    const token = jwt.sign(
+      {
+        email: existingUser.email,
+        id: existingUser._id,
+        role: existingUser.role,
+      },
+      process.env.JWT_SECRET!,
+      { expiresIn: "7d" },
+    );
+
+    res.cookie("token", token, {
+      sameSite: "lax",
+      httpOnly: true,
+      secure: false,
+      maxAge: 7 * 24 * 60 * 60 * 1000,
+    });
+    const userResponse = existingUser.toObject();
+
+    const { password: _, ...safeUser } = userResponse;
+    return res.status(200).json({
+      success: true,
+      message: "Logged in successfully",
+      user: safeUser,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      success: false,
+      message: "Internal server error",
+    });
   }
+};
 
-  const existingUser = await User.findOne({ email }).select("+password");
-  if (!existingUser) {
-    return res
-      .status(401)
-      .json({ success: false, message: "Invalid credentials" });
+export const logout = async (req: Request, res: Response) => {
+  try {
+    res.clearCookie("token", {
+      httpOnly: true,
+      sameSite: "lax",
+      secure: false,
+    });
+    return res.status(200).json({
+      success: true,
+      message: "Logged out successfully",
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: "Internal server error",
+    });
   }
-
-  const isPasswordMatch = await bcrypt.compare(password, existingUser.password);
-  if (!isPasswordMatch) {
-    return res
-      .status(401)
-      .json({ success: false, message: "Invalid credentials" });
-  }
-
-  const token = jwt.sign(
-    {
-      email: existingUser.email,
-      id: existingUser._id,
-      role: existingUser.role,
-    },
-    process.env.JWT_SECRET!,
-    { expiresIn: "7d" },
-  );
-
-  res.cookie("token", token, {
-    sameSite: "lax",
-    httpOnly: true,
-    secure: false,
-    maxAge: 7 * 24 * 60 * 60 * 1000,
-  });
-  const userResponse = existingUser.toObject();
-
-  const { password: _, ...safeUser } = userResponse;
-  return res
-    .status(200)
-    .json({ success: true, message: "Logged in successfully", user: safeUser });
 };
