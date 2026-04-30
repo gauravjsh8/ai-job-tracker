f48b4477bc0c268a201e6785dfc5dc43feed8653
gauravjsh8 <gauravjsh8@gmail.com>
2026-04-30 13:24:40 +0300

    added cloudinaryUpload functionality

---
+++ backend/src/utils/cloudinaryUpload.ts
@@ -0,0 +1,20 @@
+import { v2 as cloudinary, UploadApiResponse } from "cloudinary";
+
+import streamifier from "streamifier";
+
+export const streamUpload = (
+  buffer: Buffer,
+  folder: string,
+  resource_type: "image" | "video" | "raw",
+): Promise<UploadApiResponse> => {
+  return new Promise((resolve, reject) => {
+    const stream = cloudinary.uploader.upload_stream(
+      { folder, resource_type },
+      (error, result) => {
+        if (result) resolve(result);
+        else if (error) reject(error);
+      },
+    );
+    streamifier.createReadStream(buffer).pipe(stream);
+  });
+};
