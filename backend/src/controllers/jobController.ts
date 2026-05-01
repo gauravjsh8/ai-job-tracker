0598e320f8800d1874b9fe2ddae3c7c7a136ad79
gauravjsh8 <gauravjsh8@gmail.com>
2026-05-01 14:41:15 +0300

    added update functionality

--- backend/src/controllers/jobController.ts
+++ backend/src/controllers/jobController.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express";
 import { AuthRequest } from "../middlewares/authMiddleware";
 import Job from "../models/jobModel";
+import mongoose from "mongoose";
 
 type jobBody = {
   title: string;
@@ -8,7 +9,7 @@ type jobBody = {
   salary?: number;
   location?: string;
   notes?: string;
-  status: "applied" | "interview" | "offer" | "rejected";
+  status?: "applied" | "interview" | "offer" | "rejected";
   jobLink?: string;
 };
 export const createJob = async (
@@ -61,14 +62,76 @@ export const getJobs = async (req: AuthRequest, res: Response) => {
 export const getSingleJob = async (req: AuthRequest, res: Response) => {
   try {
     const jobId = req.params.jobId;
+
+    if (
+      !jobId ||
+      typeof jobId !== "string" ||
+      !mongoose.Types.ObjectId.isValid(jobId)
+    ) {
+      return res.status(400).json({
+        success: false,
+        message: "Invalid job ID format",
+      });
+    }
     const user = req.user?.id;
 
     if (!user) {
       return res.status(401).json({ success: false, message: "Not Logged in" });
     }
 
-    const job = await Job.findById(jobId);
+    const job = await Job.findOne({ _id: jobId, user });
+    if (!job) {
+      return res
+        .status(404)
+        .json({ success: false, message: "Job not found or unauthorized" });
+    }
     return res.status(200).json({ success: true, job });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ success: false, message: "Server error" });
+  }
+};
+
+export const updateJob = async (
+  req: AuthRequest & { body: jobBody },
+  res: Response,
+) => {
+  try {
+    const jobId = req.params.jobId;
+    const { title, company, salary, location, notes, status, jobLink } =
+      req.body;
+    const userId = req.user?.id;
+    if (!userId) {
+      return res.status(401).json({ success: false, message: "Not Logged in" });
+    }
+
+    if (
+      !jobId ||
+      typeof jobId !== "string" ||
+      !mongoose.Types.ObjectId.isValid(jobId)
+    ) {
+      return res
+        .status(400)
+        .json({ success: false, message: "Invalid job ID format" });
+    }
+    const job = await Job.findOne({ _id: jobId, user: userId });
+
+    if (!job) {
+      return res.status(404).json({ success: false, message: "Job not found" });
+    }
+
+    if (title !== undefined) job.title = title;
+    if (company !== undefined) job.company = company;
+    if (salary !== undefined) job.salary = salary;
+    if (location !== undefined) job.location = location;
+    if (notes !== undefined) job.notes = notes;
+    if (status !== undefined) job.status = status;
+    if (jobLink !== undefined) job.jobLink = jobLink;
+
+    await job.save();
+    return res
+      .status(200)
+      .json({ success: true, message: "Job updated successfully", job });
   } catch (error) {
     console.log(error);
     return res.status(500).json({ success: false, message: "Server error" });
--- backend/src/routers/jobRoutes.ts
+++ backend/src/routers/jobRoutes.ts
@@ -1,10 +1,11 @@
 import express from "express";
 import { validate } from "../middlewares/validateMiddleware";
 import { jobSchema } from "../validators/jobValidation";
-import { createJob, getJobs } from "../controllers/jobController";
+import { createJob, getJobs, getSingleJob } from "../controllers/jobController";
 import { authMiddleware } from "../middlewares/authMiddleware";
 
 export const jobRouter = express.Router();
 
 jobRouter.post("/create-job", validate(jobSchema), authMiddleware, createJob);
 jobRouter.get("/all-jobs", authMiddleware, getJobs);
+jobRouter.get("/get-single-job/:jobId", authMiddleware, getSingleJob);
