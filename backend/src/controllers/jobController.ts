aa57d99d0fd5872dbfc4cc02c223b731870265b7
gauravjsh8 <gauravjsh8@gmail.com>
2026-05-01 13:38:42 +0300

    added create job functionality  in controller

---
+++ backend/src/controllers/jobController.ts
@@ -0,0 +1,42 @@
+import { Request, Response } from "express";
+import { AuthRequest } from "../middlewares/authMiddleware";
+import Job from "../models/jobModel";
+
+type jobBody = {
+  title: string;
+  company: string;
+  salary?: number;
+  location?: string;
+  notes?: string;
+  status: "applied" | "interview" | "offer" | "rejected";
+  jobLink?: string;
+};
+export const createJob = async (
+  req: AuthRequest & { body: jobBody },
+  res: Response,
+) => {
+  try {
+    const { title, company, salary, location, notes, status, jobLink } =
+      req.body;
+    const userId = req.user?.id;
+    if (!userId) {
+      return res.status(401).json({ success: false, message: "Not Logged in" });
+    }
+
+    const job = await Job.create({
+      title,
+      salary,
+      location,
+      notes,
+      status,
+      jobLink,
+      user: userId,
+    });
+    return res
+      .status(201)
+      .json({ success: true, message: "Job created successfully", job });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ success: false, message: "Server error" });
+  }
+};
