d57148904d8bbd77e0b711eaffb731ca989a7940
gauravjsh8 <gauravjsh8@gmail.com>
2026-05-01 11:29:13 +0300

    added validate middleware

---
+++ backend/src/middlewares/validateMiddleware.ts
@@ -0,0 +1,16 @@
+import { NextFunction, Request, Response } from "express";
+import { ZodTypeAny } from "zod";
+
+export const validate =
+  (schema: ZodTypeAny) => (req: Request, res: Response, next: NextFunction) => {
+    const result = schema.safeParse(req.body);
+    if (!result.success) {
+      return res.status(400).json({
+        success: false,
+        message: result.error.issues.map((err) => err.message),
+      });
+    }
+
+    req.body = result.data;
+    next();
+  };
