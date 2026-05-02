d3f09e41fc59a0aa571db4408b8c8872e4307601
gauravjsh8 <gauravjsh8@gmail.com>
2026-05-02 16:06:07 +0300

    added jobId ownership middleware

--- backend/src/middlewares/authMiddleware.ts
+++ backend/src/middlewares/authMiddleware.ts
@@ -1,6 +1,9 @@
 import { NextFunction, Response, Request } from "express";
 import User from "../models/userModel";
 import jwt from "jsonwebtoken";
+import { string } from "zod";
+import mongoose from "mongoose";
+import Job from "../models/jobModel";
 
 export interface AuthRequest extends Request {
   user?: { id: string; role: "user" | "admin"; email: string };
@@ -31,5 +34,45 @@ export const authMiddleware = async (
     next();
   } catch (error) {
     return res.status(401).json({ success: false, message: "Token not valid" });
+  }
+};
+
+export const checkJobownership = async (
+  req: AuthRequest,
+  res: Response,
+  next: NextFunction,
+) => {
+  try {
+    const user = req.user?.id;
+    const jobId = req.params.jobId;
+
+    if (!user) {
+      return res.status(401).json({ success: false, message: "Not logged in" });
+    }
+
+    if (
+      !jobId ||
+      typeof jobId !== "string" ||
+      !mongoose.Types.ObjectId.isValid(jobId)
+    ) {
+      return res
+        .status(401)
+        .json({ success: false, message: "Invalid job id" });
+    }
+
+    const job = await Job.findById(jobId);
+    if (!job) {
+      return res.status(404).json({ success: false, message: "job not found" });
+    }
+
+    if (job.user.toString() !== user) {
+      return res.status(403).json({
+        success: false,
+        message: "Not authorized to access this job",
+      });
+    }
+    next();
+  } catch (error) {
+    return res.status(500).json({ success: false, message: "Server error" });
   }
 };
--- backend/src/routers/jobRoutes.ts
+++ backend/src/routers/jobRoutes.ts
@@ -1,11 +1,37 @@
 import express from "express";
 import { validate } from "../middlewares/validateMiddleware";
 import { jobSchema } from "../validators/jobValidation";
-import { createJob, getJobs, getSingleJob } from "../controllers/jobController";
-import { authMiddleware } from "../middlewares/authMiddleware";
+import {
+  createJob,
+  deleteJob,
+  getJobs,
+  getSingleJob,
+  jobStats,
+  monthlyStats,
+  updateJob,
+} from "../controllers/jobController";
+import {
+  authMiddleware,
+  checkJobownership,
+} from "../middlewares/authMiddleware";
 
 export const jobRouter = express.Router();
 
 jobRouter.post("/create-job", validate(jobSchema), authMiddleware, createJob);
+
 jobRouter.get("/all-jobs", authMiddleware, getJobs);
-jobRouter.get("/get-single-job/:jobId", authMiddleware, getSingleJob);
+
+jobRouter.get(
+  "/get-single-job/:jobId",
+  authMiddleware,
+  checkJobownership,
+  getSingleJob,
+);
+
+jobRouter.get("/stats", authMiddleware, jobStats);
+
+jobRouter.get("/monthly-stats", authMiddleware, monthlyStats);
+
+jobRouter.patch("/:jobId", authMiddleware, checkJobownership, updateJob);
+
+jobRouter.delete("/:jobId", authMiddleware, checkJobownership, deleteJob);
